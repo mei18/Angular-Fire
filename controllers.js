aa38f3f259701b7111e14f3357fbbfaf629b019c
Melanie Liang <melly.strearshy@gmail.com>
2016-06-22 19:09:56 -0600

    create add function

--- controllers.js
+++ controllers.js
@@ -3,10 +3,18 @@ angular.module('videoGames.controllers', ['videoGames.factories'])
 .controller('listController', function ($location, $games) {
 	var list = this;
 
-	$games.list().then(function(snapshot){
-		list.games = snapshot.val();
+	$games.list().then(function(games){
+		list.games = games;
 	}, function(error){
 		console.log(error);
 		alert(error.message);
 	});
+	list.add = function () {
+		$games.add(list.game).then(function() {
+			alert('success!');
+		}, function(error){
+			alert('error');
+			console.log(error);
+		});
+	};
 });
--- factories.js
+++ factories.js
@@ -5,17 +5,17 @@ angular.module('videoGames.factories', [])
 
 .factory('$games', function ($firebaseArray) {
 	var games = {};
+	var ref = firebase.database().ref().child('games');
 
 	games.list = function () {
 		//me conecto a firebase como cliente
-		var ref = firebase.database().ref().child('games');
-
 		var list = firebaseArray(ref);
 		return list.$loaded();
 	};
 
-	games.add = function () {
-		
+	games.add = function (game) {
+		var list = firebaseArray(ref);
+		return list.$add(game)
 	};
 
 	return games;
